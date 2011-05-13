b1afb0de40a501e8ef5ab905577539b3b1472c8c
endeav0r <endeavor@rainbowsandpwnies.com>
2011-05-13 16:53:04 -0400

    difficult to define behavior in label.c has been cleaned up and commented

--- assembler/src/label.c
+++ assembler/src/label.c
@@ -1,15 +1,17 @@
 #include "label.h"
 
-
+// adds a new label to the end of labels and returns the new label
 struct _label * label_add (struct _label * labels, const char * text) {
     struct _label * l;
     
+    // if no labels we are already at the end. create a label
     if (labels == NULL) {
         l = (struct _label *) malloc(sizeof(struct _label));
         labels = l;
     }
     else {
         l = labels;
+        // find the end of the labels
         while (l->next != NULL) { 
             if (strcmp(l->text, text) == 0) {
                 fprintf(stderr, "found duplicate label: %s\n", text);
@@ -17,25 +19,32 @@ struct _label * label_add (struct _label * labels, const char * text) {
             }
             l = l->next;
         }
+        // create a new label at the end of these labels
         l->next = (struct _label *) malloc(sizeof(struct _label));
         l = l->next;
     }
+    // initialize this label
     l->text = (char *) malloc(strlen(text) + 1);
     strcpy(l->text, text);
+    l->type = -1;
     l->location = -1;
     l->next = NULL;
-    return labels;
+    
+    // return the new label
+    return l;
 }
 
 
 struct _label * label_get (struct _label ** labels, const char * text) {
     struct _label * label;
     
+    // if there are no labels, create one
     if (*labels == NULL) {
         *labels = label_add(*labels, text);
         label = *labels;
     }
     else {
+        // search for this label and return it if it exists
         label = *labels;
         while (1) {
             if (strcmp(label->text, text) == 0)
@@ -44,7 +53,8 @@ struct _label * label_get (struct _label ** labels, const char * text) {
             label = label->next;
         }
         
-        label = label_add(label, text);
+        // if the label doesn't exist, create a new one
+        label->next = label_add(label, text);
         label = label->next;
     }
     return label;
